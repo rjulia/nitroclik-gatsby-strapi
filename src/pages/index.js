0410f6e6ffaab32631731b2b08f591a534e838f7
Ramon Julia <reimon1972@gmail.com>
2019-10-14 19:50:19 +0800

    gatsby suit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,50 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
+import Img from 'gatsby-image'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data: { allStrapiArticle: { edges } } }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <ul>
+      {edges.map(doct => {
+        const doc = doct.node
+        return (<li key={doc.id}>
+          <h2><Link to={`/${doc.id}`}> {doc.title}</Link></h2>
+          <Img fixed={doc.image.childImageSharp.fixed} />
+          <p>{doc.content}</p>
+        </li>)
+      })}
+    </ul>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          title
+          content
+          image {
+          childImageSharp {
+              fixed (width:200, height:125) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+`
